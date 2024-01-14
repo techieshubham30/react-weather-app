dda14d990a420260753293701fab481ef78af15f
Shubham Singh <techieshubham30@gmail.com>
2024-01-15 01:10:47 +0530

    feat: add CurrentWeather component

---
+++ src/component/CurrentWeather.jsx
@@ -0,0 +1,33 @@
+import { kelvinToCelsius } from "../utils/kelvinToCelsius";
+
+const CurrentWeather = (props) => {
+  const { temp_max } = props.currentTem.day;
+  const { weather } = props.currentTem.day;
+  return (
+    <div className="current-tem-div">
+      <h1
+        className="current-tem"
+        style={{ fontSize: "3rem", fontWeight: "700" }}
+      >
+        {Math.ceil(kelvinToCelsius(temp_max))}°C
+      </h1>
+      {weather.toLowerCase() === "clouds" && (
+        <img
+          src="./assets/images/clouds.png"
+          alt="Cloudy"
+          style={{ width: "40px", height: "40px", marginLeft: "1rem" }}
+        />
+      )}
+
+      {weather.toLowerCase() === "clear" && (
+        <img
+          src="./assets/images/clear.png"
+          alt="Cloudy"
+          style={{ width: "40px", height: "40px", marginLeft: "1rem" }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CurrentWeather;
